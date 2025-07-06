d131ad8b1cc67608e9008c20f1475aa794d3ec32
ElTinmar <martin.privat@hotmail.fr>
2025-07-06 15:02:17 +0200

    fix imports

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
-from benchmarks import cpu_linear_algebra, memory_bandwidth, disk_io_benchmark
+from perf_bench import cpu_linear_algebra, memory_bandwidth, disk_io_benchmark
 
 if __name__ == "__main__":
 
     print('Running CPU benchmark')
     print(60*"=" + "\n")
-    cpu_linear_algebra.main()
+    cpu_linear_algebra()
 
     print('Running Disk IO benchmark')
     print(60*"=" + "\n")
-    disk_io_benchmark.main()
+    disk_io_benchmark()
 
     print('Running memory benchmark')
     print(60*"=" + "\n")
-    memory_bandwidth.main()
+    memory_bandwidth()
